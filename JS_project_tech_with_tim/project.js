508943a7168bce7ef4e9d549922c11a464b2c62d
Abbaasiz <abbaasibnzubair@gmail.com>
2023-11-25 13:25:25 +0000

    Update project.js to complete JS slot machine

--- JS_project_tech_with_tim/project.js
+++ JS_project_tech_with_tim/project.js
@@ -82,23 +82,109 @@ const spin = () => {
         }
     }
     console.log(symbols);
-}
 
-const reels = [];
-for (let i = 0; i < COLS; i++) {
-    reels.push([]);
-    const reelSymbols = [...symbols];
-    for (let j = 0; j < ROWS; j++) {
-        const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-        const selectedSymbol = reelSymbols[randomIndex];
-        reels[i].push(selectedSymbol);
-        reelSymbols.splice(randomIndex, 1);
+
+    const reels = [];
+
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
     }
+
     return reels;
 }
 
-spin();
-let balance = deposit();
-const lines = getNumberOfLines();
-const betAmount = getBet(balance, lines);
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
+
+const printRows = (rows) => {
+
+    for (const row of rows) {
+        let rowString = "";
+
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol;
+
+            if (i != row.length - 1) {
+                rowString += ' | ';
+            }
+
+        }
+        console.log(rowString);
+    }
+}
+
+const getWinning = (rows, bet, lines) => {
+    let winning = 0;
+
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbol[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winning += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+    return winning;
+
+}
+
+
+const game = () => {
+    let balance = deposit();
+
+    while (true) {
+        console.log("You have a balance of £" + balance);
+        const lines = getNumberOfLines();
+        const bet = getBet(balance, lines);
+        balance -= bet * lines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinning(rows, bet, lines);
+        balance += winnings;
+        console.log("You won, $" + winnings.toString());
+
+        if (balance <= 0) {
+            console.log("You have ran out of money!  Game over. ");
+            break
+        }
+
+        const playAgain = prompt("Would you like to play again? (y/n)");
+        if (playAgain != "y") break;
+    }
+
+}
+
+game();
+
+
+
+
+
+
 
